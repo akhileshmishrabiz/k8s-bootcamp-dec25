10baa2d2712b55e49398e179616665b674626dee
Devops with Akhilesh <livingdevops@gmail.com>
2026-01-24 11:18:59 +0530

    infra for class 11

---
+++ eks-proper-infra/infra/load-balancer-controller.tf
@@ -0,0 +1,54 @@
+
+# IAM role and policy for AWS Load Balancer Controller
+
+
+# helm chart deploymnet for aws load balancer controller
+
+# Install AWS Load Balancer Controller using Helm
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.8.1"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_load_balancer_controller.arn
+  }
+
+  set {
+    name  = "region"
+    value = "ap-south-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.aws_load_balancer_controller,
+    module.eks
+  ]
+}
+
+# servive account for aws load balancer controller
+
+
+# oidc provider for irsa
